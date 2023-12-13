48836c30bb33369e478f67ad8bdf55628d689618
WestiferRobin <w.webb0919@gmail.com>
2023-12-13 15:09:46 -0800

    now working I' m adding tests

---
+++ SolarConquestIntegrationTests/BattleTests/PrismBattleTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolarConquestIntegrationTests.BattleTests
+{
+    internal class PrismBattleTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ SolarConquestUnitTests/Prism/PrismUnitTest.cs
@@ -0,0 +1,16 @@
+namespace SolarConquestUnitTests.Prism
+{
+    public class PrismUnitTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
