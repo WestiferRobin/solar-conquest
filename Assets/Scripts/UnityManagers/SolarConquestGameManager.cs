fa366e44c5e97116683c7ad8fe58cd596a184158
Wesley  Webb <wesleywebb@Wesleys-MBP.lan>
2023-12-24 04:06:44 -0800

    added tilemap and finished folder refactor for high priority into unity integration

---
+++ Assets/Scripts/UnityManagers/SolarConquestGameManager.cs
@@ -0,0 +1,35 @@
+using Levithan;
+using SolarConquestGameModels;
+using SoverignParticles;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class SolarConquestGameManager : MonoBehaviour
+{
+    public GameSystem LevithanSystem;
+    public IGame SolarConquest;
+
+    public Particle PrismID = Particle.Omega;
+
+    void Start()
+    {
+        LevithanSystem = new LevithanGameSystem();
+        SolarConquest = LevithanSystem.Game;
+        LevithanSystem.StartGame();
+        //Debug.Log(system.Owner.Name);
+        //var 
+        //solarConquest = new SolarConquestGame(
+        //    new PrismPlayer(system.Owner.AvatarPrism),
+        //    new PrismPlayer(system.OwnerHedron.GetPrism(PrismID))
+        //);
+        //system.LaunchGame(solarConquest);
+    }
+
+    void Update()
+    {
+        // This is where our controller affects the game model
+    }
+}
---
+++ Assets/Scripts/UnityManagers/UnityModelManager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class UnityModelManager : MonoBehaviour
+{
+    private SolarConquestGameManager GameManager;
+
+    public TileBase baseTile;
+    public TileBase eventTile;
+    public TileBase grassTile;
+    public TileBase highlightTile; // The highlight Tile to use
+    public TileBase wallTile;
+    public TileBase waterTile;
+    private Tilemap tilemap; // Reference to your Tilemap
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (gameObject.TryGetComponent<SolarConquestGameManager>(out var manager))
+        {
+            this.GameManager = manager;
+            this.tilemap = GetComponent<Tilemap>();
+
+            for (int y = 0; y < 5; y++)
+            {
+                for (int x = 0; x < 5; x++)
+                {
+                    this.tilemap.SetTile(
+                        new Vector3Int(x, y),
+                        baseTile
+                    );
+                }
+            }
+        }
+        else throw new Exception("UNITY MODEL MANAGER IS BROKEN!");
+    }
+
+    void Update()
+    {
+
+        var gameBoard = GameManager.SolarConquest.Board;
+
+        foreach (var line in gameBoard.GetLines())
+        {
+            foreach (var item in line.GetLineItems())
+            {
+                Debug.Log(item);
+            }
+        }
+
+        // Read from the data model of SolarConquest
+        // Will need to do this for tiles and other shit
+        // Anyways main idea is that Unity visualise our data
+        // That data will handle encounters
+        // This game must have a strong mod support
+        // BECAUSE we can make openai services
+        // For rapid prototyping and test data generation
+        // We can create ai generated random events and shit
+        // FUCKING RANDOM FUN CREATIVE AND EXCITING
+    }
+}
